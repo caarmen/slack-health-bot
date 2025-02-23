5e800cb23456ebdfa62968aa14cf6c70210c3488
Carmen Alvarez <carmen@rmen.ca>
2025-02-23 20:19:44 +0100

    Avoid duplicate Slack messages.
    Sometimes, fitbit or withings notify us multiple times, for the same event, within the same second or so.
    
    We already have some logic inside the notification webhook routes to avoid processing duplicate events:
    1 Check if the event was marked as processed.
    2 If marked as processed, exit.
    3 Process the event.
    4 Mark the event as processed.
    
    The problem is: During "process the event", we get another call on our webhook. We haven't processed it, so we haven't yet marked it as processed. So, it gets processed twice, and the Slack message gets sent twice.
    
    This commit only addresses the slack message duplication.
    
    We need to think more about avoiding processing the duplicated events in the first place.
    * If we mark the event as processed before actually starting to process it, if an exception occurs during the processing, the next duplicate event becomes a missed opportunity: we could have been able to try to process it again, but we won't.
    * We could add a mutex (asyncio Lock) around the object that holds the info about the event being processed or not, but this might make our route slower to respond.
    
    We'll (maybe) think about handling this more cleanly at the source (in the webhook notification), but for now, this commit just prevents duplicate slack messages being posted.

--- slackhealthbot/remoteservices/api/slack/messageapi.py
+++ slackhealthbot/remoteservices/api/slack/messageapi.py
@@ -1,3 +1,8 @@
+import datetime as dt
+import logging
+from asyncio import Lock
+from dataclasses import dataclass
+
 import httpx
 from dependency_injector.wiring import Provide, inject
 from fastapi import Depends
@@ -6,11 +11,37 @@ from slackhealthbot.containers import Container
 from slackhealthbot.settings import Settings
 
 
+@dataclass
+class LastMessage:
+    message: str | None = None
+    time_sent: dt.datetime | None = None
+
+
+last_message = LastMessage()
+
+last_message_lock = Lock()
+
+MESSAGE_DUPLICATION_WINDOW_S = 5
+
+
 @inject
 async def post_message(
     message: str,
     settings: Settings = Depends(Provide[Container.settings]),
 ):
+    # Band-aid quick workaround to avoid sending duplicate messages to Slack.
+    # Skip this message if we already sent it "recently".
+    now = dt.datetime.now(dt.timezone.utc)
+    async with last_message_lock:
+        if (
+            last_message
+            and last_message.message == message
+            and (now - last_message.time_sent).seconds < MESSAGE_DUPLICATION_WINDOW_S
+        ):
+            logging.warning(f"Ignoring duplicate message {last_message}")
+            return
+        last_message.message = message
+        last_message.time_sent = now
     async with httpx.AsyncClient(
         timeout=settings.app_settings.request_timeout_s,
         transport=httpx.AsyncHTTPTransport(
