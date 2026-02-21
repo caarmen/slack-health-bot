d1e50f52ced50b3e5566096794cef1550c17cf8e
Carmen Alvarez <carmen@rmen.ca>
2026-02-21 19:19:22 +0100

    refactor(tests): Move some tests out of test_usecase_post_activity and into test_usecase_activity_message_formatter.

--- tests/domain/usecases/slack/test_usecase_post_activity.py
+++ tests/domain/usecases/slack/test_usecase_post_activity.py
@@ -11,91 +11,9 @@ from slackhealthbot.domain.models.activity import (
     ActivityZoneMinutes,
     TopActivityStats,
 )
-from slackhealthbot.domain.usecases.slack import (
-    usecase_activity_message_formatter,
-    usecase_post_activity,
-)
+from slackhealthbot.domain.usecases.slack import usecase_post_activity
 from slackhealthbot.main import app
 from slackhealthbot.settings import AppSettings, SecretSettings, Settings
-
-
-@pytest.mark.parametrize(
-    [
-        "input_value",
-        "expected_output",
-    ],
-    [
-        (0, "➡️"),
-        (1, "➡️"),
-        (-1, "➡️"),
-        (100, "⬆️"),
-        (-100, "⬇️"),
-        (5, "↗️"),
-        (-5, "↘️"),
-    ],
-)
-def test_get_activity_minutes_change_icon(
-    input_value: int,
-    expected_output: str,
-):
-    actual_output = usecase_activity_message_formatter.get_activity_minutes_change_icon(
-        input_value
-    )
-    assert actual_output == expected_output
-
-
-@pytest.mark.parametrize(
-    [
-        "input_value",
-        "expected_output",
-    ],
-    [
-        (0, "➡️"),
-        (1, "➡️"),
-        (-1, "➡️"),
-        (100, "⬆️"),
-        (-100, "⬇️"),
-        (35, "↗️"),
-        (-35, "↘️"),
-    ],
-)
-def test_get_activity_calories_change_icon(
-    input_value: int,
-    expected_output: str,
-):
-    actual_output = (
-        usecase_activity_message_formatter.get_activity_calories_change_icon(
-            input_value
-        )
-    )
-    assert actual_output == expected_output
-
-
-@pytest.mark.parametrize(
-    [
-        "input_value",
-        "expected_output",
-    ],
-    [
-        (0, "➡️"),
-        (1, "➡️"),
-        (-1, "➡️"),
-        (30, "⬆️"),
-        (-30, "⬇️"),
-        (20, "↗️"),
-        (-20, "↘️"),
-    ],
-)
-def test_get_activity_distance_km_change_icon(
-    input_value: int,
-    expected_output: str,
-):
-    actual_output = (
-        usecase_activity_message_formatter.get_activity_distance_km_change_icon(
-            distance_km_change_pct=input_value
-        )
-    )
-    assert actual_output == expected_output
 
 
 @pytest.mark.parametrize(
